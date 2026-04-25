305a7401b32fed8218a7a51b5d5e3e2752687ffc
zenfurion <roman2004berdin@gmail.com>
2026-04-25 18:26:45 +0300

    commit

---
+++ random_fact.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import random
+
+git_hub = "https://github.com/berdinrr/practica_4.5/tree/main"
+
+facts = [
+   "Бананы радиоактивны и излучают небольшое количество гамма-излучения.",
+   "Человек может обойтись без пищи до 2 месяцев, а без воды — всего несколько дней.",
+   "Голубые киты ежедневно потребляют около 4 тонн пищи.",
+   "Пчёлы могут распознавать лица людей.",
+   "Существует 6 000 видов бананов, а не только один."
+]
+
+def show_fact():
+        random_fact = random.choice(facts)
+        fact_label.config(text=random_fact)
+
+window = tk.Tk()
+window.title("Случайный факт")
+window.geometry("500x250")
+
+tk.Label(window, text="Нажми на кнопку, чтобы получить рандомный факт").pack(pady=10)
+
+fact_button = tk.Button(
+        window,
+        text="Показать факт",
+        command=show_fact
+)
+fact_button.pack(pady=10)
+
+fact_label = tk.Label(window, text="Здесь появится факт")
+fact_label.pack(pady=20)
+
+window.mainloop()
